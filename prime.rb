7e7e3065bc0ec62639f4acaa9b76ae10538e791d
Mike Barberry <mikebarberry@protonmail.com>
2020-10-08 03:47:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
 def prime?(num)
+  if num < 0 or num == 0 or num == 1
+      return false
+  else
   n = 2
  while n < num
    return false if num % n == 0
    n += 1
  end
  true
+ end 
 end
 
 prime?(-1)
